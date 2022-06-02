a4e59c38036fd3a8f39a8cb23d38ed05c6e31c29
Cr7stian8 <cristianotropeiro@email.com>
2022-06-02 14:28:55 -0300

    "Adição @Repository"

--- app03/src/main/java/com/farmacia/app03/repository/ProdutoRepository.java
+++ app03/src/main/java/com/farmacia/app03/repository/ProdutoRepository.java
@@ -7,10 +7,11 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.farmacia.app03.model.Produto;
 
-
+@Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long >{
 	public List<Produto> findAllByvalorContainingIgnoreCase (String valor);
 
