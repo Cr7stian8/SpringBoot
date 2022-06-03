d1d6c462a3c777a837ea444ae07f89fc42be45a2
Cr7stian8 <cristianotropeiro@email.com>
2022-06-03 11:58:21 -0300

    Adição projeto inicial Backend Loja de games

---
+++ app04/src/test/java/com/generation/app04/App04ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.app04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class App04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
